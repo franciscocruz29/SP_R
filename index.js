88599b7818f5618b1573d99dce2862b45d04cb87
franciscocruz29 <franciscohuertas25@gmail.com>
2023-04-05 11:14:52 -0500

    Completed problem

--- index.js
+++ index.js
@@ -6,3 +6,46 @@ function doSearch(query) {
     .then(response => response.json())
     .then(result => result.hits);
 }
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      input: '',
+      list: []
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    doSearch(this.state.input)
+      .then((hits) => this.setState({ list: hits }));
+  }
+
+  onChange(e) {
+    this.setState({ input: e.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Search Hacker News with React</h1>
+        <form type="submit" onSubmit={this.onSubmit}>
+          <input type="text" onChange={this.onChange} value={this.state.input}/>
+          <button type="text">Search</button>
+        </form>
+
+        {this.state.list.map(item => <div key={item.objectID}>{item.title}</div>)}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
